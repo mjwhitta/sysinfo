f62d0035237bdea9597ba7173e3798b75f71ded7
Miles Whittaker <mj@whitta.dev>
2021-01-23 11:44:23 -0500

    Added macOS and Windows support

--- globals.go
+++ globals.go
@@ -1,4 +1,4 @@
 package sysinfo
 
 // Version is the package version
-const Version = "1.1.10"
+const Version = "1.2.0"
--- sysinfo.go
+++ sysinfo.go
@@ -2,16 +2,13 @@ package sysinfo
 
 import (
 	"encoding/json"
-	"io/ioutil"
+	"net"
 	"os"
 	"os/exec"
-	"regexp"
-	"strconv"
 	"strings"
 	"sync"
 
 	hl "gitlab.com/mjwhitta/hilighter"
-	"gitlab.com/mjwhitta/pathname"
 	"gitlab.com/mjwhitta/where"
 )
 
@@ -24,6 +21,8 @@ type SysInfo struct {
 	Height      int    `json:"-"`
 	HomeFS      string `json:"HomeFS,omitempty"`
 	Host        string `json:"Host,omitempty"`
+	ipMutex     *sync.Mutex
+	ips         map[string][]string
 	IPv4        string `json:"IPv4,omitempty"`
 	IPv6        string `json:"IPv6,omitempty"`
 	Kernel      string `json:"Kernel,omitempty"`
@@ -40,7 +39,7 @@ type SysInfo struct {
 // New will return a SysInfo pointer. A list of fields can be
 // supplied if all info is not wanted.
 func New(fields ...string) *SysInfo {
-	var s = &SysInfo{}
+	var s = &SysInfo{ipMutex: &sync.Mutex{}}
 
 	s.order = fields
 	if len(fields) == 0 {
@@ -84,6 +83,7 @@ func (s *SysInfo) Clear() {
 	s.CPU = ""
 	s.HomeFS = ""
 	s.Host = ""
+	s.ips = nil
 	s.IPv4 = ""
 	s.IPv6 = ""
 	s.Kernel = ""
@@ -209,51 +209,6 @@ func (s *SysInfo) Collect() {
 	s.calcSize()
 }
 
-func (s *SysInfo) colors() string {
-	s.Colors = strings.Join(
-		[]string{
-			hl.Hilights([]string{"light_black", "on_black"}, "▄▄▄"),
-			hl.Hilights([]string{"light_red", "on_red"}, "▄▄▄"),
-			hl.Hilights([]string{"light_green", "on_green"}, "▄▄▄"),
-			hl.Hilights([]string{"light_yellow", "on_yellow"}, "▄▄▄"),
-			hl.Hilights([]string{"light_blue", "on_blue"}, "▄▄▄"),
-			hl.Hilights(
-				[]string{"light_magenta", "on_magenta"},
-				"▄▄▄",
-			),
-			hl.Hilights([]string{"light_cyan", "on_cyan"}, "▄▄▄"),
-			hl.Hilights([]string{"light_white", "on_white"}, "▄▄▄"),
-		},
-		"",
-	)
-	return s.Colors
-}
-
-func (s *SysInfo) cpu() string {
-	var e error
-	var info []byte
-	var r *regexp.Regexp
-
-	if info, e = ioutil.ReadFile("/proc/cpuinfo"); e != nil {
-		s.CPU = ""
-		return s.CPU
-	}
-
-	r = regexp.MustCompile(`(cpu model|model name)\s+:\s+(.+)`)
-	for _, match := range r.FindAllStringSubmatch(string(info), -1) {
-		s.CPU = match[2]
-		break
-	}
-
-	r = regexp.MustCompile(`\((R|TM)\)| (@|CPU)`)
-	s.CPU = r.ReplaceAllString(s.CPU, "")
-
-	r = regexp.MustCompile(`\s+`)
-	s.CPU = r.ReplaceAllString(s.CPU, " ")
-
-	return s.CPU
-}
-
 func (s *SysInfo) exec(cmd string, cli ...string) string {
 	var e error
 	var o []byte
@@ -267,25 +222,6 @@ func (s *SysInfo) exec(cmd string, cli ...string) string {
 	}
 
 	return strings.TrimSpace(string(o))
-}
-
-func (s *SysInfo) filesystems() []string {
-	var out = []string{}
-
-	s.RootFS = s.fsUsage("/")
-	s.HomeFS = s.fsUsage("/home")
-
-	if len(s.RootFS) > 0 {
-		out = append(out, s.RootFS)
-	}
-
-	if (len(s.HomeFS) > 0) && (s.HomeFS != s.RootFS) {
-		out = append(out, s.HomeFS)
-	} else {
-		s.HomeFS = ""
-	}
-
-	return out
 }
 
 func (s *SysInfo) format(k string, v string, max int) string {
@@ -303,148 +239,105 @@ func (s *SysInfo) format(k string, v string, max int) string {
 	return line
 }
 
-func (s *SysInfo) fsUsage(path string) string {
-	var usage string
-	var words []string
+func (s *SysInfo) getIPs() map[string][]string {
+	var addrs []net.Addr
+	var e error
+	var ifaces []net.Interface
+	var ip net.IP
+	var tmp string
 
-	usage = s.exec("df", "-h", path)
+	s.ipMutex.Lock()
+	defer s.ipMutex.Unlock()
 
-	for _, line := range strings.Split(usage, "\n") {
-		words = strings.Fields(line)
-		if (len(words) == 6) && (words[5] == path) {
-			return words[2] + " / " + words[1] + " (" + words[4] + ")"
+	if (s.ips != nil) && (len(s.ips) > 0) {
+		return s.ips
+	}
+
+	s.ips = map[string][]string{}
+
+	if ifaces, e = net.Interfaces(); e != nil {
+		return s.ips
+	}
+
+	for _, iface := range ifaces {
+		if addrs, e = iface.Addrs(); e != nil {
+			continue
+		}
+
+		for _, addr := range addrs {
+			tmp = addr.String()
+			tmp = tmp[0:strings.Index(tmp, "/")]
+
+			if ip = net.ParseIP(tmp); (ip == nil) || ip.IsLoopback() {
+				continue
+			}
+
+			if ip.IsLinkLocalMulticast() || ip.IsLinkLocalUnicast() {
+				continue
+			}
+
+			s.ips[iface.Name] = append(
+				s.ips[iface.Name],
+				addr.String(),
+			)
 		}
 	}
 
-	return ""
+	return s.ips
 }
 
 func (s *SysInfo) hostname() string {
 	var e error
-	var host []byte
 
-	host, e = ioutil.ReadFile("/proc/sys/kernel/hostname")
-	if e == nil {
-		s.Host = strings.TrimSpace(string(host))
+	if s.Host, e = os.Hostname(); e != nil {
+		s.Host = ""
 	}
 
 	return s.Host
 }
 
 func (s *SysInfo) ipv4() string {
-	var dev string
-	var matches [][]string
-	var r *regexp.Regexp
+	var ip net.IP
+	var tmp string
 
-	r = regexp.MustCompile(`^default.+dev\s+(\S+)`)
-	matches = r.FindAllStringSubmatch(s.exec("ip", "-o", "r"), -1)
-	for _, match := range matches {
-		dev = match[1]
-	}
+	for _, vs := range s.getIPs() {
+		for _, v := range vs {
+			tmp = v[0:strings.Index(v, "/")]
 
-	r = regexp.MustCompile(`(?i)\s+inet\s+(\S+)`)
-	matches = r.FindAllStringSubmatch(
-		s.exec("ip", "-o", "a", "s", dev),
-		-1,
-	)
-	for _, match := range matches {
-		s.IPv4 = match[1]
-		break
+			if ip = net.ParseIP(tmp); ip == nil {
+				continue
+			}
+
+			if ip.To4() != nil {
+				s.IPv4 = v
+				return s.IPv4
+			}
+		}
 	}
 
 	return s.IPv4
 }
 
 func (s *SysInfo) ipv6() string {
-	var dev string
-	var matches [][]string
-	var r *regexp.Regexp
+	var ip net.IP
+	var tmp string
 
-	r = regexp.MustCompile(`^default.+dev\s+(\S+)`)
-	matches = r.FindAllStringSubmatch(s.exec("ip", "r"), -1)
-	for _, match := range matches {
-		dev = match[1]
-		break
-	}
+	for _, vs := range s.getIPs() {
+		for _, v := range vs {
+			tmp = v[0:strings.Index(v, "/")]
 
-	r = regexp.MustCompile(`(?i)\s+inet6\s+(\S+)`)
-	matches = r.FindAllStringSubmatch(
-		s.exec("ip", "-o", "a", "s", dev),
-		-1,
-	)
-	for _, match := range matches {
-		s.IPv6 = match[1]
-		break
-	}
+			if ip = net.ParseIP(tmp); ip == nil {
+				continue
+			}
 
-	r = regexp.MustCompile(`(?i)^fe[89ab]`)
-	if r.MatchString(s.IPv6) {
-		s.IPv6 = ""
+			if ip.To4() == nil {
+				s.IPv6 = v
+				return s.IPv6
+			}
+		}
 	}
 
 	return s.IPv6
-}
-
-func (s *SysInfo) kernel() string {
-	var e error
-	var kernel []byte
-
-	kernel, e = ioutil.ReadFile("/proc/sys/kernel/osrelease")
-	if e == nil {
-		s.Kernel = strings.TrimSpace(string(kernel))
-	}
-
-	return s.Kernel
-}
-
-func (s *SysInfo) operatingSystem() string {
-	var e error
-	var matches [][]string
-	var r *regexp.Regexp
-	var release []byte
-
-	if pathname.DoesExist("/etc/os-release") {
-		if release, e = ioutil.ReadFile("/etc/os-release"); e != nil {
-			s.OS = ""
-			return s.OS
-		}
-
-		r = regexp.MustCompile(`PRETTY_NAME="(.+)"`)
-		matches = r.FindAllStringSubmatch(string(release), -1)
-		for _, match := range matches {
-			s.OS = match[1] + " " + s.exec("uname", "-m")
-			break
-		}
-	} else {
-		s.OS = s.exec("uname", "-m", "-s")
-	}
-
-	return s.OS
-}
-
-func (s *SysInfo) ram() string {
-	var matches [][]string
-	var r *regexp.Regexp
-	var total int
-	var used int
-
-	r = regexp.MustCompile(`Mem:\s+(\d+)\s+(\d+)`)
-	matches = r.FindAllStringSubmatch(s.exec("free"), -1)
-	for _, match := range matches {
-		total, _ = strconv.Atoi(match[1])
-		used, _ = strconv.Atoi(match[2])
-
-		total /= 1024
-		used /= 1024
-
-		s.RAM = strconv.Itoa(used) + " MB"
-		s.RAM += " / "
-		s.RAM += strconv.Itoa(total) + " MB"
-
-		break
-	}
-
-	return s.RAM
 }
 
 // SetDataColors will set the color values for the field data. See
@@ -457,17 +350,6 @@ func (s *SysInfo) SetDataColors(colors ...string) {
 // gitlab.com/mjwhitta/hilighter for valid colors.
 func (s *SysInfo) SetFieldColors(colors ...string) {
 	s.fieldColors = colors
-}
-
-func (s *SysInfo) shell() string {
-	var exists bool
-	var sh string
-
-	if sh, exists = os.LookupEnv("SHELL"); exists {
-		s.Shell = strings.TrimSpace(sh)
-	}
-
-	return s.Shell
 }
 
 // String will convert the SysInfo struct to a printable string.
@@ -492,7 +374,7 @@ func (s *SysInfo) String() string {
 		case "Blank":
 			out = append(out, "")
 		case "Colors":
-			if len(s.Colors) > 0 {
+			if s.Colors != "" {
 				out = append(out, " "+s.Colors)
 			}
 		case "FS":
@@ -513,49 +395,4 @@ func (s *SysInfo) String() string {
 	}
 
 	return strings.Join(out, "\n")
-}
-
-func (s *SysInfo) tty() string {
-	var e error
-	if s.TTY, e = os.Readlink("/proc/self/fd/0"); e != nil {
-		s.TTY = ""
-	}
-	return s.TTY
-}
-
-func (s *SysInfo) uptime() string {
-	var r *regexp.Regexp
-
-	s.Uptime = s.exec("uptime")
-
-	// Fail fast
-	if s.Uptime == "" {
-		return s.Uptime
-	}
-
-	// Strip extra whitespace
-	r = regexp.MustCompile(`\s+`)
-	s.Uptime = r.ReplaceAllString(s.Uptime, " ")
-
-	// Strip leading and trailing data
-	r = regexp.MustCompile(`^.*up\s+|,\s+\d+\s+user.+$`)
-	s.Uptime = r.ReplaceAllString(s.Uptime, "")
-
-	// Convert hours:mins to match days
-	r = regexp.MustCompile(`0?(\d+):0?(\d+)`)
-	s.Uptime = r.ReplaceAllString(s.Uptime, "$1 hour, $2 min")
-
-	// Remove 0 hours and mins
-	r = regexp.MustCompile(`(^|,\s+)0\s+(hour|min)`)
-	s.Uptime = r.ReplaceAllString(s.Uptime, "")
-
-	// Make plural if needed
-	r = regexp.MustCompile(`((\d\d+|[2-9])\s+(hour|min))`)
-	s.Uptime = r.ReplaceAllString(s.Uptime, "${1}s")
-
-	// Remove leading and trailing commas or whitespace
-	r = regexp.MustCompile(`^(,?\s*)+|(,?\s*)+$`)
-	s.Uptime = r.ReplaceAllString(s.Uptime, "")
-
-	return s.Uptime
 }
