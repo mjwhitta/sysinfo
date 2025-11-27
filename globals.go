183aa6f12456b5c9212f19e435fe255859e852af
Miles Whittaker <mj@whitta.dev>
2025-11-26 20:18:23 -0500

    Updated deps

--- globals.go
+++ globals.go
@@ -3,7 +3,7 @@ package sysinfo
 import "regexp"
 
 // Version is the package version
-const Version string = "1.7.2"
+const Version string = "1.7.3"
 
 var (
 	reCommaSpace *regexp.Regexp = regexp.MustCompile(
